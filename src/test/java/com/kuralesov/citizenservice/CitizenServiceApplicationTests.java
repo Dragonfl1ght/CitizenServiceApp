af173cef0a5cd3be2c706266c537e325f858b161
Egor <egor.kuralesov@yandex.ru>
2023-11-15 19:52:07 +0300

    Initial commit

---
+++ src/test/java/com/kuralesov/citizenservice/CitizenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuralesov.citizenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
