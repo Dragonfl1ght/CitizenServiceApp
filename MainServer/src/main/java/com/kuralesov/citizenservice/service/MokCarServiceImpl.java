b9da698685a163c58427f2990a407d630404fe49
Dragonfl1gt <egor.kuralesov@yandex.ru>
2024-05-03 10:25:13 +0300

    package com.kuralesov error

---
+++ MainServer/src/main/java/com/kuralesov/citizenservice/service/MokCarServiceImpl.java
@@ -0,0 +1,36 @@
+package com.kuralesov .citizenservice.service;
+
+import com.kuralesov.citizenservice.model.Car;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Profile("Mok")
+public class MokCarServiceImpl implements CarService{
+    @Override
+    public Car create(Car car) {
+        throw new RuntimeException("create");
+    }
+
+    @Override
+    public Car getById(Long id) {
+        throw new RuntimeException("getById");
+    }
+
+    @Override
+    public List<Car> getAll() {
+        throw new RuntimeException("getAll");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        throw new RuntimeException("deleteById");
+    }
+
+    @Override
+    public Car edit(Car editRequest, Long id) {
+        throw new RuntimeException("edit");
+    }
+}
